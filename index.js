3764e85ff39ffa2d7efdb1a51c312514da562252
Blake Abel <blakecabel@gmail.com>
2019-09-16 09:54:51 -0400

    Cleanup file where the magic happens

---
+++ index.js
@@ -0,0 +1,45 @@
+// For Node.js
+const TurndownService = require('turndown')
+const fs = require('fs');
+
+let posts = fs.readdirSync('_posts')
+
+var turndownService = new TurndownService()
+
+for (let i = 0; i < 1; i++) {
+    let contents = fs.readFileSync(`_posts/${posts[i]}`)
+    let contentString = contents.toString()
+    let contentSplit = contentString.split('---')
+
+    let frontmatter = contentSplit[1].split('\n')
+    //console.log(frontmatter)
+    let betterFrontmatter = []
+    for(let j = 0; j < frontmatter.length; j++) {
+        if(
+            frontmatter[j].startsWith('layout') ||
+            frontmatter[j].startsWith('title') ||
+            frontmatter[j].startsWith('categories') ||
+            frontmatter[j].startsWith('tags') ||
+            frontmatter[j].startsWith('-') ||
+            frontmatter[j].startsWith('author') ||
+            frontmatter[j].startsWith('  login') ||
+            frontmatter[j].startsWith('  email') ||
+            frontmatter[j].startsWith('  display_name') ||
+            frontmatter[j].startsWith('  first_name') ||
+            frontmatter[j].startsWith('  last_name') )
+        {
+            betterFrontmatter.push(frontmatter[j])
+        }
+    }
+    //console.log(betterFrontmatter.join('\n'))
+    contentSplit[1] = '\n' + betterFrontmatter.join('\n') + '\n'
+    //Changing content to markdown
+    contentSplit[2] = '\n' + turndownService.turndown(contentSplit[2])
+    let result = contentSplit.join('---')
+    //let markdown = turndownService.turndown(contentString)
+    console.log(result)
+    console.log(`_posts/${posts[i]}`.split('.')[0] + '.md')
+    //fs.writeFileSync(`_posts/${posts[i]}`, markdown)
+}
+
+var markdown = turndownService.turndown('<h1>Hello world!</h1>')
